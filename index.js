ce7854dece223512f4c320c98cb2f64b1a7391df
thadeus <thadeusnyariki94@gmail.com>
2022-08-25 21:06:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word==="") {
+		console.log("Nothing found!");
+		return false;
+	}
+  else if(word == word.split('').reverse().join('')){
+    return true;
+  }
+  else if(word != word.split('').reverse().join('')){
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  
 */
 
 /*
   Add written explanation of your solution here
+ 1. Check whether the string is empty or not
+ 2. Compare the result of iterating normally and reverse
+ 3. if the characters match then === is palindrome will be true otherwise falsi
 */
 
 // You can run `node index.js` to view these console logs
